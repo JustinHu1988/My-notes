1666ebf233f8098a12896346c817186381cc1ffb
Justin Hu <JustinHu1988@outlook.com>
2017-04-11 19:11:08 +0800

    add

--- Code-Single-Page-Web-Applications/spa/js/spa.js
+++ Code-Single-Page-Web-Applications/spa/js/spa.js
@@ -2,16 +2,17 @@
  *spa.js
  *Root namespace module
  */
-/* jslint              browser:ture,   continue:true,
-devel:true,        indent:4,          maxerr:50,
-newcap:true,    nomen:true,     plusplus:true,
-regexp:true,     sloppy:true,      vars:false,
-white:true
-*/
+/* jslint       browser:ture,   continue:true,
+ devel:true,    indent:4,       maxerr:50,
+ newcap:true,   nomen:true,     plusplus:true,
+ regexp:true,   sloppy:true,    vars:false,
+ white:true
+ */
 /*global $, spa */
 
-var spa = (function(){
-    var initModule = function($container){
+var spa = (function () {
+    'use strict';
+    var initModule = function ($container) {
         $container.html(
             '<h1 style="display:inline-block;margin:25px">'
             + 'hello world!'
@@ -19,5 +20,11 @@ var spa = (function(){
         );
     };
     return {initModule: initModule};
-})();
+}());
+
+
+
+
+
+
 
---
+++ Code-Single-Page-Web-Applications/spa/js/spa.shell.js
@@ -0,0 +1,34 @@
+/*
+ * spa.shell.js
+ * Shell module for SPA
+ */
+/* jslint       browser:ture,   continue:true,
+ devel:true,    indent:4,       maxerr:50,
+ newcap:true,   nomen:true,     plusplus:true,
+ regexp:true,   sloppy:false,    vars:false,
+ white:true
+ */
+/*global $, spa */
+
+spa.shell = (function () {
+    
+    var configMap = {
+        main_html: String()
+        + '<div class="spa-shell-head">'
+        + '<div class="spa-shell-head-logo"></div>'
+        + '<div class="spa-shell-head-acct"></div>'
+        + '<div class="spa-shell-head-search"></div>'
+        + '</div>'
+        + '<div class="spa-shell-main">'
+        + '<div class="spa-shell-main-nav"></div>'
+        + '<div class="spa-shell-main-content"></div>'
+        + '</div>'
+        + '<div class="spa-shell-foot"></div>'
+        + '<div class="spa-shell-chat"></div>'
+        + '<div class="spa-shell-modal"></div>'
+    };
+});
+
+
+
+
