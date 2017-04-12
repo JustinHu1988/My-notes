ed41c8c51c843a63e74f815767f17d1b2d6fe39d
Justin Hu <JustinHu1988@outlook.com>
2017-04-12 16:39:46 +0800

    add

--- Code-Single-Page-Web-Applications/spa/js/spa.js
+++ Code-Single-Page-Web-Applications/spa/js/spa.js
@@ -2,21 +2,14 @@
  *spa.js
  *Root namespace module
  */
-/* jslint       browser:ture,   continue:true,
- devel:true,    indent:4,       maxerr:50,
- newcap:true,   nomen:true,     plusplus:true,
- regexp:true,   sloppy:true,    vars:false,
- white:true
- */
-/*global $, spa */
 
 var spa = (function () {
     'use strict';
     var initModule = function ($container) {
         $container.html(
-            '<h1 style="display:inline-block;margin:25px">'
-            + 'hello world!'
-            + '</h1>'
+            '<h1 style="display:inline-block;margin:25px">' +
+            'hello world!' +
+            '</h1>'
         );
     };
     return {initModule: initModule};
