95facea5af93bcc4a89e29b5fd2e0bea04121c4f
Justin Hu <JustinHu1988@outlook.com>
2017-04-14 01:36:15 +0800

    add

--- nodejs/example-01.js
+++ nodejs/example-01.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res)=>{
     });
 
 server.listen(port, hostname, ()=>{
-    console.log('Server running at http://127.0.0.1:3000/');
+    console.log(`Server running at http:/${hostname}:${port}/`);
 });
---
+++ nodejs/work001/hello/main.js
@@ -0,0 +1,8 @@
+`use strict`;
+
+// 引入hello module
+var greet = require(`./hello`);
+
+var s = `Michael`;
+
+greet(s);
