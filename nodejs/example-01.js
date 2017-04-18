a47a3d1f6397fbb44f08759d2b20c498633dcc69
Justin Hu <JustinHu1988@outlook.com>
2017-04-18 11:14:33 +0800

    add

--- nodejs/example-01.js
+++ nodejs/example-01.js
@@ -1,8 +1,12 @@
+`use strict`;
+
+// 导入http模块：
 const http = require(`http`);
 
 const hostname = `127.0.0.1`;
 const port = 3000;
 
+// 创建http server，并传入回调函数
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
     res.setHeader(`Content-Type`,`text/plain`);
